0d77f48ca9fd3974d551968b3356ccf06511c46e
Bownze <lindamayisela27@gmail.com>
2023-08-08 05:36:34 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -28,12 +28,18 @@ char *str_concat(char *s1, char *s2)
 	conct = malloc(sizeof(char) * (i + ci + 1));
 
 	if (conct == NULL)
-		return (NULL)
+		return (NULL);
 	i = ci = 0;
 	while (s1[i] != '\0')
 	{
-		conct[i] = s1[i]
+		conct[i] = s1[i];
 			i++;
+	}
+	while (s2[ci] != '\0')
+	{
+		conct[i] = s2[ci];
+		i++;
+		ci++;
 	}
 	conct[i] = '\0'
 		return (conct);
