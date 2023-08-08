7b87724856e06e40ee5ad5ea03f0fc3ef5e70adf
Bownze <lindamayisela27@gmail.com>
2023-08-08 13:20:46 -0700

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -7,7 +7,7 @@
  * @av: double pointer array
  * Return: 0
  */
-char *argstostr(int ac, char *av)
+char *argstostr(int ac, char **av)
 {
 	int i, n, r = 0, l = 0;
 	char *str;
@@ -34,7 +34,8 @@ char *argstostr(int ac, char *av)
 			r++;
 		}
 		if (str[r] == '\0')
-		{str[r++] = '\n';
+		{
+			str[r++] = '\n';
 		}
 	}
 	return (str);
